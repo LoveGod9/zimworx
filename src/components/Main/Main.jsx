5772130297946aa7d1151cf59b6ee8e8b582d0d7
Tatenda Lance Ganyani <tatendaloveganyani@gmail.com>
2026-02-27 12:45:16 +0200

    Employee

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -20,7 +20,7 @@ const ExpenseTracker = () => {
 
   return (
     <Card className={classes.root}>
-      <CardHeader title="Zimworx Employee Expense Tracker" subheader="Powered by Tatenda Lance Ganyani" />
+      <CardHeader title="Employee Expense Tracker" subheader="Powered by Tatenda Lance Ganyani" />
       <CardContent>
         <Typography align="center" variant="h5">
           Total Balance ${balance}
